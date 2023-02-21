58c0889c18c69476a91250503ff3ca4ffd3e9550
ctrlmelive <118881107+ctrlmelive@users.noreply.github.com>
2023-02-21 01:04:27 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # set val
 python3 /app/self-ping.py &
 
-AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+AUUID=52fabe93-03d3-4e9e-b7a4-6213ecd34189
 ParameterSSENCYPT=chacha20-ietf-poly1305
 CADDYIndexPage=https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
 
